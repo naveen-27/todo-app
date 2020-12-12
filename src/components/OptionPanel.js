aab4051d3f49945c72a99c351e4fef16b1e88575
Naveen Kumar <456endson@gmail.com>
2020-12-12 22:17:48 +0530

    Finished filter todos by given options

---
+++ src/components/OptionPanel.js
@@ -0,0 +1,46 @@
+import React from "react";
+import classes from "../stylesheets/OptionPanel.module.css";
+
+const OptionPanel = (props) => {
+  return (
+    <div className={classes.Panel}>
+      <p>{props.remainingTodos} items left</p>
+
+      <div className={classes.filter}>
+        <button
+          tabIndex={0}
+          className={`${classes.option} ${
+            props.filterApplied === "all" ? classes.active : ""
+          }`}
+          onClick={() => props.changeFilter("all")}
+        >
+          All
+        </button>
+        <button
+          tabIndex={0}
+          className={`${classes.option} ${
+            props.filterApplied === "active" ? classes.active : ""
+          }`}
+          onClick={() => props.changeFilter("active")}
+        >
+          Active
+        </button>
+        <button
+          tabIndex={0}
+          className={`${classes.option} ${
+            props.filterApplied === "completed" ? classes.active : ""
+          }`}
+          onClick={() => props.changeFilter("completed")}
+        >
+          Completed
+        </button>
+      </div>
+
+      <button onClick={props.clearCompleted} className={classes.btn}>
+        Clear Completed
+      </button>
+    </div>
+  );
+};
+
+export default OptionPanel;
