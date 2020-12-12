80e84c81b8da90f7447575fbad204ade42889487
Naveen Kumar <456endson@gmail.com>
2020-12-12 22:19:09 +0530

    Added help and citations

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -4,12 +4,15 @@ import Header from "./Header";
 import TodoInput from "./TodoInput";
 import getBackgroundImage from "../utilites/GetBackgroundImage";
 import classes from "../stylesheets/TodoApp.module.css";
+import Todos from "./Todos";
+import OptionPanel from "./OptionPanel";
 
 const TodoApp = () => {
   const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
   const [todos, setTodos] = useState(
     JSON.parse(localStorage.getItem("todos")) || []
   );
+  const [filter, setFilter] = useState("all");
 
   const toggleTheme = () => {
     const changedTheme = theme === "light" ? "dark" : "light";
@@ -18,6 +21,8 @@ const TodoApp = () => {
   };
 
   const addTodo = (todoContent) => {
+    if (todoContent === "") return;
+
     const newTodo = {
       id: uuid(),
       todo: todoContent,
@@ -27,6 +32,52 @@ const TodoApp = () => {
     const newTodos = [...todos, newTodo];
     setTodos(newTodos);
     localStorage.setItem("todos", JSON.stringify(newTodos));
+  };
+
+  const toggleDone = (idx) => {
+    const modifiedTodos = [...todos];
+    modifiedTodos[idx].isDone = !modifiedTodos[idx].isDone;
+    setTodos(modifiedTodos);
+    localStorage.setItem("todos", JSON.stringify(modifiedTodos));
+  };
+
+  const deleteTodo = (idx) => {
+    const modifiedTodos = [
+      ...todos.slice(0, idx),
+      ...todos.slice(idx + 1, todos.length),
+    ];
+    setTodos(modifiedTodos);
+    localStorage.setItem("todos", JSON.stringify(modifiedTodos));
+  };
+
+  const changeFilter = (option) => {
+    setFilter(option);
+  };
+
+  const filterTodo = () => {
+    if (filter === "active") {
+      return todos.filter((todo) => !todo.isDone);
+    } else if (filter === "completed") {
+      return todos.filter((todo) => todo.isDone);
+    } else {
+      return todos;
+    }
+  };
+
+  const clearCompleted = () => {
+    const modifiedTodos = todos.filter((todo) => !todo.isDone);
+    setTodos(modifiedTodos);
+    localStorage.setItem("todos", JSON.stringify(modifiedTodos));
+  };
+
+  const getRemainingTodos = () => {
+    return todos.reduce((remaining, todo) => {
+      if (!todo.isDone) {
+        return remaining + 1;
+      } else {
+        return remaining;
+      }
+    }, 0);
   };
 
   return (
@@ -39,7 +90,31 @@ const TodoApp = () => {
     >
       <div className={classes["Todo-wrapper"]}>
         <Header theme={theme} themeToggleFn={toggleTheme} />
+
         <TodoInput addTodo={addTodo} />
+
+        <Todos
+          todos={filterTodo()}
+          toggleDone={toggleDone}
+          delete={deleteTodo}
+        />
+
+        <OptionPanel
+          clearCompleted={clearCompleted}
+          changeFilter={changeFilter}
+          remainingTodos={getRemainingTodos()}
+          filterApplied={filter}
+        />
+
+        <p className={classes.help}>Drag and drop to reorder list.</p>
+
+        <p className={classes.citation}>
+          Challenge by{" "}
+          <a href="https://www.frontendmentor.io/?ref=challenge">
+            Frontend Mentor
+          </a>
+          . Coded by <a href="https://www.github.com/naveen-27">Naveen</a>.
+        </p>
       </div>
     </div>
   );
