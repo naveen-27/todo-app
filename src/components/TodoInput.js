cb009fb5d75b2df91525e76e536233004714d2cc
Naveen Kumar <456endson@gmail.com>
2020-12-12 13:11:27 +0530

    Styled Input component and basic interactivity features

---
+++ src/components/TodoInput.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import classes from "../stylesheets/TodoInput.module.css";
+
+const TodoInput = (props) => {
+  const [todo, setTodo] = useState("");
+
+  const handleChange = (e) => {
+    setTodo(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    props.addTodo(todo);
+    setTodo("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className={classes.Form}>
+      <input
+        className={classes.Input}
+        type="text"
+        name="todo"
+        value={todo}
+        placeholder="Create a new todo..."
+        onChange={handleChange}
+      />
+    </form>
+  );
+};
+
+export default TodoInput;
