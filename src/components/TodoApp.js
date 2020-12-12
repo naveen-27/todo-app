9453ffc0f012d65f152408d4d032855b74c57d99
Naveen Kumar <456endson@gmail.com>
2020-12-12 13:11:55 +0530

    Made significant changes in functionality
    1) Added Input Component
    2) Fixed state initialising to string if local storage key exists --> parsed string to JSON
    3) Added AddTodo logic

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -1,15 +1,32 @@
 import React, { useState } from "react";
+import { v4 as uuid } from "uuid";
 import Header from "./Header";
+import TodoInput from "./TodoInput";
 import getBackgroundImage from "../utilites/GetBackgroundImage";
 import classes from "../stylesheets/TodoApp.module.css";
 
 const TodoApp = () => {
   const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
+  const [todos, setTodos] = useState(
+    JSON.parse(localStorage.getItem("todos")) || []
+  );
 
   const toggleTheme = () => {
     const changedTheme = theme === "light" ? "dark" : "light";
     localStorage.setItem("theme", changedTheme);
     setTheme(changedTheme);
+  };
+
+  const addTodo = (todoContent) => {
+    const newTodo = {
+      id: uuid(),
+      todo: todoContent,
+      isDone: false,
+    };
+
+    const newTodos = [...todos, newTodo];
+    setTodos(newTodos);
+    localStorage.setItem("todos", JSON.stringify(newTodos));
   };
 
   return (
@@ -22,6 +39,7 @@ const TodoApp = () => {
     >
       <div className={classes["Todo-wrapper"]}>
         <Header theme={theme} themeToggleFn={toggleTheme} />
+        <TodoInput addTodo={addTodo} />
       </div>
     </div>
   );
