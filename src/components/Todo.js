760738b0a76ed69f9499efb8e9478056450f7062
Naveen Kumar <456endson@gmail.com>
2020-12-12 22:17:13 +0530

    Finished styling, layout, delete and toggleDone

---
+++ src/components/Todo.js
@@ -0,0 +1,68 @@
+import React from "react";
+import { ReactComponent as Check } from "../images/icon-check.svg";
+import Cross from "../images/icon-cross.svg";
+import classes from "../stylesheets/Todo.module.css";
+
+const Todo = (props) => {
+  const { todo, isDone } = props.todo;
+  const todoCheckedClassName = isDone ? classes.checked : "";
+  const todoCompletedClassName = isDone ? classes.done : "";
+
+  const isClickedOrEnterPressed = (e) => {
+    const isEnterKeyPressed = e.type === "keydown" && e.keyCode === 13;
+    const isMouseClicked = e.type === "click";
+
+    return isEnterKeyPressed || isMouseClicked;
+  };
+
+  const handleToggleClick = (e) => {
+    if (isClickedOrEnterPressed(e)) {
+      props.toggleDone(props.index);
+    }
+  };
+
+  const handleDelete = (e) => {
+    if (isClickedOrEnterPressed(e)) {
+      e.target.parentElement.classList.add(classes.deleting);
+    }
+  };
+
+  const deleteTodo = (e) => {
+    if (/.*_fadeOut_/.test(e.animationName)) {
+      props.delete(props.index);
+    }
+  };
+
+  return (
+    <div className={classes.Todo} onAnimationEnd={deleteTodo}>
+      <div
+        className={classes.checkbox}
+        tabIndex="0"
+        onClick={handleToggleClick}
+        onKeyDown={handleToggleClick}
+      >
+        <div className={`${classes.check} ${todoCheckedClassName}`}>
+          <Check />
+        </div>
+      </div>
+
+      <p
+        className={`${classes["todo-content"]} ${todoCompletedClassName}`}
+        onClick={handleToggleClick}
+      >
+        {todo}
+      </p>
+
+      <img
+        src={Cross}
+        alt=""
+        className={classes.delete}
+        tabIndex="0"
+        onClick={handleDelete}
+        onKeyDown={handleDelete}
+      />
+    </div>
+  );
+};
+
+export default Todo;
